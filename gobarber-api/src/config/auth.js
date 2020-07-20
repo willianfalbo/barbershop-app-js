07368b24a6a7af9d88a013fc260d52ab171bf37b
willianfalbo <willianfalbo@gmail.com>
2020-07-20 19:54:33 -0300

    removed older project

--- gobarber-api/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: 'b6c0b36f37f38f308f0b74127009a209', // secret or private key
-  expiresIn: '7d',
-};
--- gobarber-api/src/server.js
+++
@@ -1,6 +0,0 @@
-import app from './app';
-
-const port = 3333;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
