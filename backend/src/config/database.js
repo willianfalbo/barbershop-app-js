1497431576cacd230bb59d2d4e4461637b27a56e
willianfalbo <willianfalbo@gmail.com>
2020-07-29 19:46:05 -0300

    added mongodb and docker-compose file

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,13 @@
 // this file should use the common js syntax e.g. "require" and "module.exports"
 // because the sequelize interface can not read the es5+ "import" and "export" syntax
 
+// TODO: add this to env config
 module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
   password: 'password',
-  database: 'barbershop-js',
+  database: 'barbershop-db-js',
   define: {
     // to add createAt/updatedAt columns in each table
     timestamps: true,
