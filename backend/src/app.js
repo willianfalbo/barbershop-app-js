3030b80f1a0f2e393c9ba2fbbc496a2ccd56e12c
willianfalbo <willianfalbo@gmail.com>
2020-08-01 01:47:48 -0300

    added error monitoring with sentry.io & exception handler middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,31 @@
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
-
-import './database'; // calls index.js by default
+import sentryConfig from './config/sentry';
+import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
-    this.server.use(express.json()); // to enable json body requests
+    // sentry request handler must be the first middleware on the app
+    this.server.use(Sentry.Handlers.requestHandler());
+
+    // to enable json on body requests
+    this.server.use(express.json());
+
+    // to serve static files
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp'))
@@ -22,6 +34,16 @@ class App {
 
   routes() {
     this.server.use(routes);
+    // sentry error handler must be before any other error middleware and after all controllers
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      // Youch is a pretty error reporting
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,5 @@
+// TODO: add this to env config
+export default {
+  dsn:
+    'https://2c0f2896b1be47b6a8fdf85d468da0c1@o428430.ingest.sentry.io/5373887',
+};
