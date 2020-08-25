9d1bf5032c46d5ecc8675cec92e382de5a6f10b2
willianfalbo <willianfalbo@gmail.com>
2020-08-25 18:28:34 -0300

    frontend: configured private routes

---
+++ frontend/src/routes/Route.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signedIn = false;
+
+  if (!signedIn && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signedIn && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
