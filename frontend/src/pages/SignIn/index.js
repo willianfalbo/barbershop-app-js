fc3cba2ae4518c8bc825c5b334290da51298e45e
willianfalbo <willianfalbo@gmail.com>
2020-09-04 00:30:17 -0300

    frontend: added unform lib

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Form, Input } from '@rocketseat/unform';
 import logo from '../../assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img className="logo" src={logo} alt="Barbershop" />
 
-      <form>
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
         <button type="submit">Sign In</button>
         <Link to="/register">Create an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Form, Input } from '@rocketseat/unform';
 import logo from '../../assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+
   return (
     <>
       <img className="logo" src={logo} alt="Barbershop" />
 
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Create Account Now</button>
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Name" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+        <button type="submit">Create Account</button>
         <Link to="/">Already have an account?</Link>
-      </form>
+      </Form>
     </>
   );
 }
