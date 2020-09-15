2ecb14a28148f61c09f996285a92d4e5303c18b7
willianfalbo <willianfalbo@gmail.com>
2020-09-15 01:03:37 -0300

    frontend: updating header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo-black.svg';
@@ -8,6 +9,8 @@ import Notifications from '../Notifications';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector((state) => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -21,12 +24,15 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Willian Falbo</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">My Profile</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Willian Falbo"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
