8714b81354a3110d9e582b49e20ca9d21926689c
willianfalbo <willianfalbo@gmail.com>
2020-09-14 21:40:30 -0300

    frontend: created profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) { }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="E-mail Address" />
+        <hr />
+        <Input name="oldPassword" type="password" placeholder="Old Password" />
+        <Input name="password" type="password" placeholder="New Password" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Password Confirmation"
+        />
+        <button type="submit">Update Profile</button>
+      </Form>
+      <button type="button">Logout</button>
+    </Container>
+  );
 }
 
 export default Profile;
