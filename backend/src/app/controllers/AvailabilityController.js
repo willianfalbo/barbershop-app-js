88f04392d4c22f851ab9abf4a6b40158ea73cd55
willianfalbo <willianfalbo@gmail.com>
2020-09-18 01:19:50 -0300

    backend: abstracting availability list with service class

--- backend/src/app/controllers/AvailabilityController.js
+++ backend/src/app/controllers/AvailabilityController.js
@@ -1,15 +1,5 @@
-import {
-  startOfDay,
-  endOfDay,
-  setHours,
-  setMinutes,
-  setSeconds,
-  format,
-  isAfter,
-} from 'date-fns';
-import { Op } from 'sequelize';
-import Appointment from '../models/Appointment';
 import { BadRequestException } from '../errors';
+import AvailabilityListService from '../services/AvailabilityListService';
 
 class AvailableController {
   async list(req, res) {
@@ -21,45 +11,9 @@ class AvailableController {
 
     const searchDate = Number(date);
 
-    const appointments = await Appointment.findAll({
-      where: {
-        provider_id: req.params.id,
-        canceled_at: null,
-        date: {
-          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
-        },
-      },
-    });
-
-    // TODO: Move it to a shared file
-    const schedule = [
-      '08:00',
-      '09:00',
-      '10:00',
-      '11:00',
-      '12:00',
-      '13:00',
-      '14:00',
-      '15:00',
-      '16:00',
-      '17:00',
-      '18:00',
-      '19:00',
-    ];
-
-    const availability = schedule.map(time => {
-      const [hour, minute] = time.split(':');
-      const sanitizedDate = setSeconds(
-        setMinutes(setHours(searchDate, hour), minute),
-        0
-      );
-      return {
-        time,
-        value: format(sanitizedDate, "yyyy-MM-dd'T'HH:mm:ssxxx"),
-        available:
-          isAfter(sanitizedDate, new Date()) &&
-          !appointments.find(a => format(a.date, 'HH:mm') === time),
-      };
+    const availability = await AvailabilityListService.run({
+      providerId: req.params.id,
+      searchDate,
     });
 
     return res.json(availability);
